77ce21cc4caac486b75c2622e44930710a5a17c3
StingRaptor <develop.state@gmail.com>
2016-04-11 14:28:29 +0200

    Fix problem with incorrect namespace used
    * Removed incorrect Processing tag from namespace

--- VisualProgrammer/Properties/Settings.Designer.cs
+++ VisualProgrammer/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VisualProgrammer.Processing.Properties {
+namespace VisualProgrammer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
